a5ec8aa24af4dd51ae9a3e08d9705c4708e1cbaf
changsanjiang <changsanjiang@gmail.com>
2017-12-25 00:19:56 +0800

    Update Demo

--- Demo/SJLoadingViewProject/ViewController.m
+++ Demo/SJLoadingViewProject/ViewController.m
@@ -24,6 +24,8 @@
     
     _loadingView = [SJLoadingView new];
     _loadingView.lineColor = [UIColor whiteColor];
+    _loadingView.bounds = CGRectMake(0, 0, 50, 50);
+    _loadingView.center = self.view.center;
     _loadingView.speed = 1;
     [self.view addSubview:_loadingView];
     
