0f17f26864230d55a177bb0cd2ddc2904c29fe16
changsanjiang <changsanjiang@gmail.com>
2017-12-24 13:54:54 +0800

    Initial Commit

---
+++ SJLoadingViewProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SJLoadingViewProject
+//
+//  Created by 畅三江 on 2017/12/24.
+//  Copyright © 2017年 畅三江. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SJLoadingViewProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SJLoadingViewProject
+//
+//  Created by 畅三江 on 2017/12/24.
+//  Copyright © 2017年 畅三江. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
